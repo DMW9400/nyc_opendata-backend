a5b3275c99ccebe46a184c4a841f8e65928180c3
github username <github email address>
2018-09-20 15:09:33 -0400

    creating correct geoJSON hashes

--- app/trial_file.rb
+++ app/trial_file.rb
@@ -10,19 +10,26 @@ require './geojson.rb'
 #   end
 # end
 
-http = Curl.get("https://data.cityofnewyork.us/resource/ah89-62h9.json?$$app_token=#{your token here}")
+# http = Curl.get("https://data.cityofnewyork.us/resource/ah89-62h9.json?$$app_token=#{your token here}")
 
-parsed = JSON.parse(http.body_str)
+# parsed = JSON.parse(http.body_str)
 
-borough_array = []
+# borough_array = []
+#
+# parsed.each do |item|
+#   if item["geo_type_name"] == 'UHF42'
+#     borough_array.push(item)
+#   end
+#   borough_array
+# end
+#
+# puts borough_array
 
-parsed.each do |item|
-  if item["geo_type_name"] == 'UHF42'
-    borough_array.push(item)
-  end
-  borough_array
+Coordinates::GeoJSON[:features].each do |thing|
+  return_obj = {}
+  return_obj[:uhf_neigh] = thing[:properties][:uhf_neigh]
+  return_obj[:uhf_code] = thing[:properties][:uhfcode]
+  return_obj[:geoJSON] = thing
+  # puts 'this thing'
+  puts return_obj
 end
-
-puts borough_array
-
-# puts Coordinates::GeoJSON
