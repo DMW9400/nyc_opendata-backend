11a7cead6bf06a8d587f1d93aeee17e694b03e5a
github username <github email address>
2018-09-20 15:22:18 -0400

    initial migration files

--- db/migrate/20180917174155_create_regions.rb
+++ db/migrate/20180917174155_create_regions.rb
@@ -1,6 +1,9 @@
 class CreateRegions < ActiveRecord::Migration[5.1]
   def change
     create_table :regions do |t|
+      t.string :name
+      t.integer :uhf_code
+      t.string :geoJSON
 
       t.timestamps
     end
--- db/migrate/20180917174908_create_metrics.rb
+++ db/migrate/20180917174908_create_metrics.rb
@@ -1,6 +1,10 @@
 class CreateMetrics < ActiveRecord::Migration[5.1]
   def change
     create_table :metrics do |t|
+      t.string :name
+      t.string :measure
+      t.string :data_value
+      t.string :year
 
       t.timestamps
     end
