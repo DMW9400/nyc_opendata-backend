76ac699b6bacd3078d55d1de4c330b47b1a99e19
github username <github email address>
2018-09-23 17:20:11 -0400

    working on pulling geoJson formatted correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,15 @@ require 'json'
 
 # require '../geojson.rb'
 
-http = Curl.get("https://data.cityofnewyork.us/resource/ah89-62h9.json?$$app_token=")
+http = Curl.get("https://data.cityofnewyork.us/resource/ah89-62h9.json?$$app_token=LUEOzBK6IUAyZVp7zYDxuvZht")
 parsed = JSON.parse(http.body_str)
 
 uhf42_metrics = []
 parsed.each do |item|
   if item["geo_type_name"] == 'UHF42'
     # uhf42_metrics.push(item)
-    Metric.create
+    Metric.create(name: item["name"], measure: item[""])
+    puts item["name"]
   end
 end
 
