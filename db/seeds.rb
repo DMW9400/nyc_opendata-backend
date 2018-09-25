a072c2f2b98a923ffe7279787657f7cbd60f9acc
github username <github email address>
2018-09-24 20:33:35 -0400

    hacking away

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,16 @@ geo_array.each do |geo_item|
 end
 
 UHF_Metrics::Borough_Array.each do |metric|
-  found_region_id = nil
+  metric_id = metric["geo_entity_id"]
   Region.all.each do |region|
+    puts  region.uhf_code
+    # puts `metric id: #{metric["geo_entity_id"]}, region id: #{region.uhf_code}`
     if region.uhf_code == metric["geo_entity_id"]
-      found_region_id = region.id
+      return 'MATCH'
     end
   end
-  Metric.create(name: metric["name"], measure: metric["measure"], data_value: metric["data_valuemessage"], year: metric["year_description"], region_code: metric["geo_entity_id"], region_id: found_region_id )
+  # puts found_region_id
+  # Metric.create(name: metric["name"], measure: metric["measure"], data_value: metric["data_valuemessage"], year: metric["year_description"], region_code: metric["geo_entity_id"], region_id: found_region_id )
 end
 
 
