0fc024f3a400a566534ba882fdcea647b9824ea9
github username <github email address>
2018-09-24 20:47:51 -0400

    added index to controllers

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -3,5 +3,6 @@ class MetricsController < ApplicationController
   def index
         artists = Metric.all
         render json: metrics, status: 200
-      end
+  end
+
 end
--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,2 +1,8 @@
 class RegionsController < ApplicationController
+
+  def index
+        artists = Region.all
+        render json: regions, status: 200
+  end
+
 end
